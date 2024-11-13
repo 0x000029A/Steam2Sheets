eae292d92358f6040088597fbf02d1be0e3903d1
Mahmoud Hashem <89657916+0x000029A@users.noreply.github.com>
2024-11-13 03:41:06 +0200

    Update Steam2Sheets.user.js

--- Steam2Sheets.user.js
+++ Steam2Sheets.user.js
@@ -1,36 +1,36 @@
 // ==UserScript==
 // @name         Steam2Sheets
 // @namespace    http://tampermonkey.net/
-// @version      v24.11.11
+// @version      24.11.12
 // @description  Add Steam games to Google Sheets.
 // @author       mHashem
 // @match        https://store.steampowered.com/app/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steampowered.com
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
+// @tag          automation
 // ==/UserScript==
 
 (async function() {
     'use strict';
 
-    const gameName = document.getElementById("appHubAppName").innerText;
+    const gameName = document.querySelector("#appHubAppName").innerText;
     const steamID = window.location.href.match(/\/app\/(\d+)\//)[1];
 
-    const IGDBID = await getIGDBID(steamID);
+    const IGDBID = await getIGDBID();
 
     if (IGDBID) {
         var addGameBtnDiv = createAddGameBtnDiv();
-        observeDOM('#queueActionsCtn', (parentDiv) => {
-            parentDiv.insertBefore(addGameBtnDiv, parentDiv.querySelector('div:nth-child(7)'));
-        });
-        if (await googleApps(1)) return;
-        else addGameBtnDiv.addEventListener('mousedown', () => googleApps(2), {once: true});
+        const parentDiv = await observeDOM('#queueActionsCtn');
+        parentDiv.insertBefore(addGameBtnDiv, parentDiv.querySelector('div:nth-child(7)'));
+        if (await googleApps(true)) return;
+        else addGameBtnDiv.addEventListener('mousedown', () => googleApps(false), {once: true});
     }
     ///////////////////////
     function createAddGameBtnDiv() {
         const addGameBtnDiv__ = document.createElement('div');
         addGameBtnDiv__.id = 'addGameBtnDiv';
-        addGameBtnDiv__.innerHTML = `<p id="addGameBtn"><span class="material-symbols-outlined">check_box_outline_blank</span>Add Game</p>`;
+        addGameBtnDiv__.innerHTML = `<p><span class="material-symbols-outlined">check_box_outline_blank</span>Add Game</p>`;
 
         GM_addStyle(`
         @import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0&icon_names=check_box,check_box_outline_blank");
@@ -44,15 +44,15 @@
             cursor: pointer;
             user-select: none;
             }
-            #addGameBtn {
+            #addGameBtnDiv p {
             padding: 7px;
-                color: #61bff7;
-                background-color: #274157;
-                border-radius: 2px;
+            color: #61bff7;
+            background-color: #274157;
+            border-radius: 2px;
             }
-            #addGameBtn:hover {
-                background-image: linear-gradient(to right, #66bff2, #427d9e);
-                color:white;
+            #addGameBtnDiv p:hover {
+            background-image: linear-gradient(to right, #66bff2, #427d9e);
+            color:white;
             }
             .material-symbols-outlined {
             float:left;
@@ -63,7 +63,7 @@
 
         return addGameBtnDiv__;
     }
-    async function getIGDBID(uid) {
+    async function getIGDBID() {
         return new Promise((resolve, reject) => {
             GM.xmlHttpRequest({
                 method: 'POST',
@@ -74,7 +74,7 @@
                     'Accept': 'application/json',
                     'Content-Type': 'text/plain'
                 },
-                data: `fields game; where uid = "${uid}" & category = 1;`,
+                data: `fields game; where uid = "${steamID}" & category = 1;`,
                 onload: function(response) {
                     if (response.status === 200) {
                         resolve(JSON.parse(response.responseText)[0].game);
@@ -90,25 +90,28 @@
             });
         });
     }
-    function observeDOM(selector, callback) {
-        const targetNode = document.querySelector(selector);
-        if (targetNode) {
-            callback(targetNode);
-        } else {
-            const observer = new MutationObserver(() => {
-                const target = document.querySelector(selector);
-                if (target) {
-                    observer.disconnect();
-                    callback(target);
-                }
-            });
-            observer.observe(document.body, { childList: true, subtree: true });
-        }
+    async function observeDOM(selector) {
+        return new Promise((resolve) => {
+            const targetNode = document.querySelector(selector);
+            if (targetNode) {
+                resolve(targetNode);
+            } else {
+                const observer = new MutationObserver(() => {
+                    const target = document.querySelector(selector);
+                    if (target) {
+                        observer.disconnect();
+                        resolve(target);
+                    }
+                });
+                observer.observe(document.body, { childList: true, subtree: true });
+            }
+        });
     }
-    function googleApps(opcode) {
+    function googleApps(checkOnly) {
         let data;
-        if (opcode == "1") data = `gameID=${encodeURIComponent(IGDBID)}&gameName=${encodeURIComponent(gameName)}`;
-        else if (opcode == "2") data = `gameID=${encodeURIComponent(IGDBID)}&accessToken=${encodeURIComponent("access_token")}`
+        if (checkOnly) data = `IGDBID=${encodeURIComponent(IGDBID)}&gameName=${encodeURIComponent(gameName)}`;
+        else if (!checkOnly) data = `IGDBID=${encodeURIComponent(IGDBID)}&accessToken=${encodeURIComponent("access_token")}`
+
         return new Promise((resolve, reject) => {
             GM.xmlHttpRequest({
                 method: 'POST',
@@ -120,7 +123,6 @@
                         addGameBtnDiv.querySelector("span").innerHTML = "check_box";
                         addGameBtnDiv.querySelector("p").style.background = "#4882a6";
                         addGameBtnDiv.style.cursor = "not-allowed";
-                        addGameBtnDiv.removeEventListener('mousedown', googleApps);
                         console.log("Game already exist");
                         resolve(true);
                     } else if (response.responseText === "Success"){
